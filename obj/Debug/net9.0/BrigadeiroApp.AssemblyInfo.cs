f31dc3dcaa123504b9ada273a3ce9a0e02cc7afc
paulooliveira152012 <paulooliveira152012@gmail.com>
2025-09-04 20:04:30 -0300

    gitignore adicionado e lançamento concluido pelo github

--- obj/Debug/net9.0/BrigadeiroApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/BrigadeiroApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrigadeiroApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b9648f6ff70f34769c058e92ad1af67eed6daaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87ea68b07baa33f99ad221d8fc59d57890871b82")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrigadeiroApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrigadeiroApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
